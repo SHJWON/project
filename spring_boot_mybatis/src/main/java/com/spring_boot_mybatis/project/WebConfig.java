053e66bf6169acbabd1919e64cc21f7a981da01a
SHJWON <sjw3344@gmail.com>
2023-02-13 16:37:30 +0900

    hi

--- spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/WebConfig.java
+++ spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:///C:/springWorkspace/product_images/",
+		
+		.addResourceLocations("file:///usr/local/project/product_images/",
+		//.addResourceLocations("file:///C:/springWorkspace/product_images/",
 				              "file:///C:/springWorkspace/upload/");
 	
 		registry.addResourceHandler("/audio/**")
